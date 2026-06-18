7e6d4aba8b08d11838bc9ad941662f6b74a08c52
sayandweep <sayandweepbiswas@gmail.com>
2026-06-18 20:44:03 +0530

    20:43

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,11 @@ function App() {
           Evnin <br />Agency
         <div className="degnE"><img src="/evnin.webp" alt="ev-logo" /></div>
         </h1>
-        <div className="reviews overflow-hidden w-full">
+        <div className="reviews overflow-x-hidden w-full">
           <div className='maRq'>
             {reviews.map(review => 
             <div className='block whitespace-break-spaces mx-1 text-[.7em] px-4 rounded-md border border-zinc-800 font-1 shadow-2xl text-left w-50' key={review.id}>
-              <div className="nM -mb-1 pt-1 text-[.7em] flex items-center">{review.name} ✦ <span className='inline-block ml-1'>{review.company}</span></div>
+              <div className="nM -mb-1 pt-1 text-[.7em] flex items-center">{review.name} ✦ <span className='inline-block'>{review.company}</span></div>
               <div className="rV pb-2 text-white">{review.content}</div>
             </div>  
             )}
@@ -21,7 +21,7 @@ function App() {
         </div>
         <div className="reels">
           <div className="grid grid-cols-3 gap-4 w-150">
-            {reels.map(reel => <div key={reel.id}><video src={reel.src} autoPlay muted className='rounded-md' loop></video></div>)}
+            {reels.map(reel => <div key={reel.id}><video src={reel.src} autoPlay muted className='rounded-md' preload="metadata" loop></video></div>)}
           </div>
         </div>
       </div>
